ce88163404a27889047d880002af47638b258cf7
Vilius Grigaliūnas <vilius.grigaliunas@cinemaads.lt>
2020-05-06 11:53:33 +0300

    Refactored registration test
    Moved to functional test namespace, added test for duplicate email.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @inheritDoc
+     */
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+
         $manager->flush();
+    }
+
+    /**
+     * Create sample users.
+     *
+     * @param ObjectManager $manager
+     */
+    private function loadUsers(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('test-user@example.com');
+        $user->setPassword(
+            '$argon2id$v=19$m=65536,t=4,p=1$kwagWtiC+ScCfYuVCZRLuw$Pb7GJ7W8xs98M2L5BEQA9sMrXbq3PsQ7f2GT4MZ+gF8'
+        ); // plain value: password123
+        $manager->persist($user);
     }
 }
---
+++ tests/Functional/RegistrationTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
+use App\Tests\DB\DatabasePrimer;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class RegistrationTest extends ApiTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DatabasePrimer::prime(self::bootKernel());
+    }
+
+    /**
+     * Test user registration.
+     *
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function testRegistrationOk(): void
+    {
+        $client = self::createClient();
+
+        $this->registerUser($client);
+        $this->assertUserAuthenticated($client);
+    }
+
+    /**
+     * Register user in the system.
+     *
+     * @param Client $client
+     * @throws TransportExceptionInterface
+     */
+    private function registerUser(Client $client): void
+    {
+        $client->request(
+            'POST',
+            '/api/users',
+            ['json' => ['email' => 'test1@example.com', 'password' => 'password1']]
+        );
+
+        self::assertResponseIsSuccessful();
+        self::assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    /**
+     * Check that user is currently authenticated.
+     *
+     * @param Client $client
+     * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    private function assertUserAuthenticated(Client $client): void
+    {
+        $client->request(
+            'GET',
+            '/api/users/me'
+        );
+
+        self::assertResponseIsSuccessful();
+        self::assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        self::assertJsonContains(['email' => 'test1@example.com']);
+    }
+
+    public function getRegistrationErrorData(): array
+    {
+        return [
+            'Empty email' => [['email' => '', 'password' => 'password1']],
+            'No fields' => [[]],
+            'Already registered' => [['email' => 'test-user@example.com', 'password' => 'pass']],
+        ];
+    }
+
+    /**
+     * Check that invalid responses result in errors.
+     *
+     * @dataProvider getRegistrationErrorData
+     * @param array $input Request content
+     * @throws TransportExceptionInterface
+     */
+    public function testRegistrationError(array $input): void
+    {
+        $client = self::createClient();
+
+        $client->request(
+            'POST',
+            '/api/users',
+            ['json' => $input]
+        );
+
+        self::assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+        self::assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+}
